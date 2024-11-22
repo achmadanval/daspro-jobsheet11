4b326d80f6407d3b15c9f9ea6db772a32455bcd2
achmadanval <anvalalan@gmail.com>
2024-11-22 10:04:30 +0700

    berubah

--- Kafe02.java
+++ Kafe02.java
@@ -1,10 +1,6 @@
 public class Kafe02 {
 
     public static void main(String[] args){
-        menu();
-    }
-    
-    public static void menu(){
         System.out.println("====== MENU RESTO KAFE ====== ");
         System.out.println("1. Kopi Hitam - Rp 15.000 ");
         System.out.println("2. Cappucino - Rp 20.000 ");
@@ -13,6 +9,6 @@ public class Kafe02 {
         System.out.println("5. Roti Bakar - Rp 10.000 ");
         System.out.println("6. Mie Goreng - Rp 18.000 ");
         System.out.println("============================ ");
-        System.out.println(" Silahkan pilih menu yang anda inginkan :) ");
-    }
+        System.out.println(" Silahkan pilih menu yang anda inginkan :)");
+    }  
 }

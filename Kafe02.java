49c06c1969b37e7de1d8c4d8c01dd28d1b85301f
achmadanval <anvalalan@gmail.com>
2024-11-22 10:41:46 +0700

    berubah lagi

--- Kafe02.java
+++ Kafe02.java
@@ -1,6 +1,22 @@
 public class Kafe02 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+         menu("Andi", true , "diskon50");
+    }
+    
+    public static void menu(String namaPelanggan, boolean isMember, String kodePromo){
+        System.out.println("Selamat datang, " +namaPelanggan + " ! ");
+        if (isMember){
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian !");
+        }
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("Anda mendapatkan diskon 50 % ");
+        }else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("Anda mendapatkan diskon sebesar 30 %");
+        }else{
+            System.out.println("Kode Invalid, masukkan lagi jika anda memiliki kode yang lain :)");
+        } 
+        
         System.out.println("====== MENU RESTO KAFE ====== ");
         System.out.println("1. Kopi Hitam - Rp 15.000 ");
         System.out.println("2. Cappucino - Rp 20.000 ");
